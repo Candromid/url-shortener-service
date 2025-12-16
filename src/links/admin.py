05de891bdb5708009302e01ea1fa109b96d13d74
Nurbek Nurdaulet <mainaccnurbek@mail.ru>
2025-12-16 11:58:28 +0500

    fix: changed file src/links/admin.py

--- src/links/admin.py
+++ src/links/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Link
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+     list_display = ('id', 'short_code', 'original_url', 'created_at')
+     search_fields = ('short_code', 'original_url')
 
 # Register your models here.
