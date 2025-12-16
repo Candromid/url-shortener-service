08ca9d13f0b59f56600f7e9f37decd54ee4772d7
Nurbek Nurdaulet <mainaccnurbek@mail.ru>
2025-12-16 15:25:39 +0500

    feat: add URL shortening API and redirect by short code

--- src/links/urls.py
+++ src/links/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import shorten_url
+from .views import shorten_url, redirect_to_original
 
 urlpatterns = [
     path('api/shorten/', shorten_url, name='shorten_url'),
+    path('<str:short_code>/', redirect_to_original, name='redirect'),
 ]
--- src/links/views.py
+++ src/links/views.py
@@ -1,48 +1,45 @@
 import json
 
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Link
 from .utils import generate_short_code
 
+from django.shortcuts import redirect, get_object_or_404
+
+
 @csrf_exempt
 def shorten_url(request):
-    if request.method != "POST":
-        return JsonResponse(
-            {"error" : "Only POST method allowed"},
-            status = 405
-        )
-    
-    try:
-        data = json.loads(request.body)
-    
-    except json.JSONDecodeError:
-        return JsonResponse(
-            {'error': 'Invalid JSON'},
-            status=400
-        )
-    
-    original_url = data.get("url")
-    
+    if request.method != 'POST':
+        return JsonResponse({'error': 'Only POST allowed'}, status=405)
+
+    data = json.loads(request.body)
+    original_url = data.get('url')
+
     if not original_url:
-        return JsonResponse(
-            {'error': 'URL is required'},
-            status=400
-        )
-        
-    short_code = generate_short_code()
-    
+        return JsonResponse({'error': 'URL is required'}, status=400)
+
     link = Link.objects.create(
-        original_url = original_url,
-        short_code = short_code
+        original_url=original_url,
+        short_code=generate_short_code()
     )
-    
-    short_url = f"http://{request.get_host()}/{link.short_code}"
-    
+
     return JsonResponse(
-        {"short_url":short_url},
-        status = 201
+        {'short_url': f"http://{request.get_host()}/{link.short_code}"},
+        status=201
     )
-# Create your views here.
+    
+    
+def redirect_to_original(request, short_code):
+    """
+    Получаем short_code из URL, ищем ссылку в БД
+    и делаем редирект на оригинальный URL
+    """
+    
+    link = get_object_or_404(Link, short_code=short_code)
+    return redirect(link.original_url)
+
+
+
+
